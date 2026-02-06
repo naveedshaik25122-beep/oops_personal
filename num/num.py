1fb46fdf83845f1e15a6ed64dce32f44f6a54aef
naveedshaik25122-beep <naveedshaik25122@gmail.com>
2026-02-06 16:19:10 +0530

    Create num.py

---
+++ num/num.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+b = np.zeros((2, 3))
+c = np.ones((3, 3))
+d = np.arange(1, 10, 2)
+e = np.linspace(0, 1, 5)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
